a2369cfab0a72b1ca29f66d3e532aeda64a59267
mrpan <mrpan@LAPTOP-ITM26DLA>
2021-08-15 01:55:17 +0700

    1

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,5 +1,18 @@
 #include<stdio.h>
 int main()
 {
+	int n;
+	printf("Enter number :");
+	scanf_s("%d", &n);
+	int x = 1;
+	while (x <= n) {
+		int y = 1;
+		while (y <= x) {
+			printf("*");
+			y += 1;
+		}
+		x += 1;
+		printf("\n");
+	}
 	return 0;
 }
